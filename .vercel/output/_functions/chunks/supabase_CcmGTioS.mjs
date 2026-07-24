42c47ac11f093ab2377e842c56f08d266488ffbc
Kjetil Hauan <kjetil@Kjetil-sin-MacBook-Air.local>
2026-07-24 19:39:19 +0200

    Fjern .vercel fra git

--- .vercel/output/_functions/chunks/supabase_CcmGTioS.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-//#region src/lib/supabase.ts
-function getSupabase() {
-	return createClient("", "");
-}
-//#endregion
-export { getSupabase as t };
